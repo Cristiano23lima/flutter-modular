f7ff9ff7691567c52f7964ee76ade15a1f2dd2d9
devCristiano <cristiano.rodrigues@f5promotora.com.br>
2020-09-04 12:46:13 -0300

    criando o model

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,13 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokemon/app/app_controller.dart';
 import 'package:pokemon/app/app_widget.dart';
 import 'package:pokemon/app/pages/home/home_controller.dart';
 import 'package:pokemon/app/pages/home/home_page.dart';
+import 'package:pokemon/app/shared/repositories/poke_repository.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
-    Bind((i) => HomeController())
+    Bind((i) => AppController()),
+    Bind((i) => HomeController()),
+    Bind((i) => PokeRepository(i.get())),
+    Bind((i) => Dio()),
   ];
 
   @override
